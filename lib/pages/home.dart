a015b39b69ae0a525e15fde13987b9a56d85582a
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-12-02 18:38:09 +0200

    customize appbar

---
+++ lib/pages/home.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.title,
+          style: TextStyle(color: Colors.lightGreen.shade800),
+        ),
+        elevation: 0.0,
+        bottom: PreferredSize(
+          child: Container(),
+          preferredSize: const Size.fromHeight(32.0),
+        ),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.lightGreen, Colors.lightGreen.shade50],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Container(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
